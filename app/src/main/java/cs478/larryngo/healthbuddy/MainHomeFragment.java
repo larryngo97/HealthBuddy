6a4d419862b9e64ffc28da16e01c6da3a0d79cbe
larryngo97 <larryngo97@gmail.com>
2019-04-23 21:29:31 -0500

    main

--- app/src/main/java/cs478/larryngo/healthbuddy/MainHomeFragment.java
+++ app/src/main/java/cs478/larryngo/healthbuddy/MainHomeFragment.java
@@ -7,12 +7,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainHomeFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_home, container, false);
+        View view = inflater.inflate(R.layout.activity_home, container, false);
+        TextView tv_layer1_statistics = (TextView) view.findViewById(R.id.home_statistics);
+        tv_layer1_statistics.setText(
+                "Today's Summary:\n" +
+                "Total Calories: 0/2000 (0.00%)\n" +
+                "Average heart rate: 89 BPM\n" +
+                "Current Weight: 120 lbs\n" +
+                "Minutes worked out: 48 minutes");
+        return view;
     }
 }
