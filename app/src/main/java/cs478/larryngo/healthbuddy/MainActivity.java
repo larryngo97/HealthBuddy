8ad25384feed107f6257ccb2c8448b2806d60563
larryngo97 <larryngo97@gmail.com>
2019-04-25 01:19:27 -0500

    design changes

--- app/src/main/java/cs478/larryngo/healthbuddy/MainActivity.java
+++ app/src/main/java/cs478/larryngo/healthbuddy/MainActivity.java
@@ -22,8 +22,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         botNavBar = findViewById(R.id.navigation_bottom);
         botNavBar.setOnNavigationItemSelectedListener(navListener);
